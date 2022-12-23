ed9c85dcd3ab373098b959c94793fe79290963d3
nbarnabee <nicole.barnabee@gmail.com>
2022-12-23 17:41:37 +0100

    Commit files modified by Vite

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-createApp(App).use(router).mount("#app");
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
