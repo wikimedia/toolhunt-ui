1f792da1d3a98ce29fa76fce5986db80b752b1eb
nbarnabee <nicole.barnabee@gmail.com>
2023-01-02 11:46:38 +0100

    Add button to see if it works

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@ import HelloWorld from "./components/HelloWorld.vue";
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <v-btn>Test</v-btn>
       </nav>
     </div>
   </header>
