757d6ede9b1339f85a20b847aa3d2c0c2dfdfff1
nbarnabee <nicole.barnabee@gmail.com>
2023-01-03 18:10:47 +0100

    Disable default css

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import "./assets/main.css";
+// import "./assets/main.css";
 import "@mdi/font/css/materialdesignicons.css";
 
 import "vuetify/styles";
