7f264946b5d377c341ecc56467dba379865645da
nbarnabee <nicole.barnabee@gmail.com>
2023-01-04 09:59:24 +0100

    Apply Vuetify tags and classes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,5 @@
 <template>
-  <footer>Toolhunt was developed as an <a href="https://www.outreachy.org/" target="_blank">Outreachy</a> internship project.  // Licensed under the  <a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="_blank">GNU GPL3</a> // <a href="https://github.com/wikimedia/toolhunt" target="_blank">Source code</a> available on GitHub // <a href="https://phabricator.wikimedia.org/tag/toolhunt/" target="_blank">Bug tracking</a> on Wikimedia Phabricator
-  </footer>
+  <v-footer color="grey-lighten-4" app>
+    <v-col class="text-center py-0 text-caption" cols="12">Toolhunt was developed as an <a href="https://www.outreachy.org/" target="_blank">Outreachy</a> internship project.  // Licensed under the  <a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="_blank">GNU GPL3</a> // <a href="https://github.com/wikimedia/toolhunt" target="_blank">Source code</a> available on GitHub // <a href="https://phabricator.wikimedia.org/tag/toolhunt/" target="_blank">Bug tracking</a> on Wikimedia Phabricator</v-col>
+  </v-footer>
 </template>
-
-<style>
-  footer {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    text-align: center;
-    background-color: #f5f5f5;
-    padding: 6px 16px;
-    line-height: 1.5;
-    font-size: 12px;
-  }
-</style>
