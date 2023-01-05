652ef9a357f46e913dc1d6b61462385dd5687100
nbarnabee <nicole.barnabee@gmail.com>
2023-01-05 07:12:59 +0100

    Split navbar component into separate file

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,12 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import Footer from "./components/Footer.vue"
+import { RouterView } from "vue-router";
+import Footer from "./components/Footer.vue";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
   <v-app>
-    <v-app-bar color="#006699">
-      <img
-        alt="Toolhunt logo"
-        class="logo"
-        src="@/assets/logo-nav.svg"
-        width="35"
-        height="35"
-      />
-      <v-app-bar-title class="text-h4">Toolhunt</v-app-bar-title>
-          <v-btn to="/">Home</v-btn>
-          <v-btn to="/dashboard">Dashboard</v-btn>
-          <v-btn to="/leaderboard">Leaderboard</v-btn>
-          <v-btn icon="mdi-account-circle" size="small"></v-btn>
-
-    </v-app-bar>
+    <Navbar />
     <v-main>
       <v-container fluid>
         <RouterView />
@@ -28,17 +15,3 @@ import Footer from "./components/Footer.vue"
     <Footer />
   </v-app>
 </template>
-
-<style>
-  .v-toolbar__content {
-    color: white;
-  }
-
-  .v-btn--icon {
-    background-color: black;
-  }
-
-  img {
-    margin-inline-start: 10px;
-  }
-</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+import { RouterLink, RouterView } from "vue-router";
+
+export default {
+<template>
+  <v-app-bar color="#006699">
+    <img
+      alt="Toolhunt logo"
+      class="logo"
+      src="@/assets/logo-nav.svg"
+      width="35"
+      height="35"
+    />
+    <v-app-bar-title class="text-h4">Toolhunt</v-app-bar-title>
+        <v-btn to="/">Home</v-btn>
+        <v-btn to="/dashboard">Dashboard</v-btn>
+        <v-btn to="/leaderboard">Leaderboard</v-btn>
+        <v-btn icon="mdi-account-circle" size="small"></v-btn>
+  </v-app-bar>
+</template>
+
+
+<style>
+  .v-toolbar__content {
+    color: white;
+  }
+
+  .v-btn--icon {
+    background-color: black;
+  }
+
+  img {
+    margin-inline-start: 10px;
+  }
+</style>
+}
