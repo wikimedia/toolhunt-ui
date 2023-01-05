bd0664de9801460d957254d6ccb76fb98ef057e5
Nicole Barnabee <95652414+nbarnabee@users.noreply.github.com>
2023-01-05 13:22:12 +0100

    Split navbar into separate component (#9)
    * Adjust vuetify tags and css styles
    
    * Split navbar component into separate file
    
    * Rewrite navbar in vanilla CSS
    
    Trying to make the Vuetify components work was just taking
    too long.  Their toolbars seem designed with a "mobile-only"
    mindset and in the end it was easier just to utilize vanilla CSS.
    It looks fine, is responsive and accessible.
    
    * Enclose icon and title in routerlink

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,12 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import Footer from "./components/Footer.vue"
+import { RouterView } from "vue-router";
+import Footer from "./components/Footer.vue";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
   <v-app>
-    <v-toolbar color="#006699">
-      <img
-        alt="Toolhunt logo"
-        class="logo"
-        src="@/assets/logo-nav.svg"
-        width="35"
-        height="35"
-      />
-      <v-toolbar-title class="text-h4">Toolhunt</v-toolbar-title>
-        <nav>
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/dashboard">Dashboard</RouterLink>
-          <RouterLink to="/leaderboard">Leaderboard</RouterLink>
-          <v-btn icon="mdi-account-circle"></v-btn>
-        </nav>
-    </v-toolbar>
+    <Navbar />
     <v-main>
       <v-container fluid>
         <RouterView />
