7eff04ea9f731298ee3b5952c75d1eee8beaae3b
nbarnabee <nicole.barnabee@gmail.com>
2023-01-05 09:54:34 +0100

    Rewrite navbar in vanilla CSS
    Trying to make the Vuetify components work was just taking
    too long.  Their toolbars seem designed with a "mobile-only"
    mindset and in the end it was easier just to utilize vanilla CSS.
    It looks fine, is responsive and accessible.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,34 +2,124 @@ import { RouterLink, RouterView } from "vue-router";
 
 export default {
 <template>
-  <v-app-bar color="#006699">
-    <img
-      alt="Toolhunt logo"
-      class="logo"
-      src="@/assets/logo-nav.svg"
-      width="35"
-      height="35"
-    />
-    <v-app-bar-title class="text-h4">Toolhunt</v-app-bar-title>
-        <v-btn to="/">Home</v-btn>
-        <v-btn to="/dashboard">Dashboard</v-btn>
-        <v-btn to="/leaderboard">Leaderboard</v-btn>
-        <v-btn icon="mdi-account-circle" size="small"></v-btn>
-  </v-app-bar>
+  <header>
+    <div class="left-side">
+      <img
+        alt="Toolhunt logo"
+        class="logo"
+        src="@/assets/logo-nav.svg"
+        width="35"
+        height="35"
+      />
+      <p class="text-h4">Toolhunt</p>
+    </div>
+    <nav class="desktop-nav" aria-label="desktop">
+      <v-btn to="/" flat>Home</v-btn>
+      <v-btn to="/dashboard" flat>Dashboard</v-btn>
+      <v-btn to="/leaderboard" flat>Leaderboard</v-btn>
+      <v-btn class="login-btn" icon="mdi-account-circle" size="small" flat></v-btn>
+    </nav>
+    <v-btn class="mobile-nav-btn" icon="mdi-menu" @click="toggleMenu" flat></v-btn>
+  </header>
+    <nav class="mobile-nav" aria-label="mobile">
+      <v-btn to="/" flat @click="toggleMenu">Home</v-btn>
+      <v-btn to="/dashboard" flat @click="toggleMenu">Dashboard</v-btn>
+      <v-btn to="/leaderboard" flat @click="toggleMenu">Leaderboard</v-btn>
+      <v-btn flat @click="toggleMenu">Login</v-btn>
+    </nav>
 </template>
 
 
+<script>
+export default {
+  methods: {
+    toggleMenu: () => {
+      document.querySelector(".mobile-nav").classList.toggle("mobile-nav-display");
+    },
+  }
+}
+</script>
+
 <style>
-  .v-toolbar__content {
+
+  header {
+    height: 64px;
+    width: 100%;
+    background-color: #006699;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .left-side {
+    display: flex;
+    gap: 10px;
+    color: white;
+  }
+  .left-side > img {
+    margin-top: 2px;
+  }
+
+  .desktop-nav {
+    display: none;
+  }
+
+  button.v-btn.login-btn {
+    background-color: black;
+  }
+
+  button.v-btn.mobile-nav-btn {
+    margin-inline-end: 10px;
+    background-color: transparent;
     color: white;
   }
 
-  .v-btn--icon {
-    background-color: black;
+  .mobile-nav {
+    display: flex;
+    flex-direction: column;
+    color: black;
+    width: 200px;
+    position: absolute;
+    top: 64px;
+    right: 0;
+    transform: translateX(200px);
+    transition: 0.2s;
+  }
+
+  .mobile-nav-display {
+    transform: translateX(0);
+    transition: 0.2s;
+  }
+
+  .mobile-nav * {
+    border-radius: 0 !important;
   }
 
   img {
     margin-inline-start: 10px;
   }
+
+  @media (min-width: 800px) {
+    .desktop-nav {
+      display: flex;
+      align-items: center;
+      margin-inline-end: 10px;
+      gap: 5px;
+    }
+
+    .desktop-nav > .v-btn {
+    background-color: transparent;
+    color: white;
+  }
+
+  .desktop-nav > .v-btn {
+    background-color: transparent;
+    color: white;
+  }
+
+    button.v-btn.mobile-nav-btn {
+      display: none;
+    }
+  } 
 </style>
 }
