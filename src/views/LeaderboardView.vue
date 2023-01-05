799f43d0831fcc023063e96207f731666cf682e4
Hannah_Waruguru <hannah.waruguru8@gmail.com>
2023-01-05 13:37:23 +0300

    Leaderboard: Add a table Add a simple table with the users and the number of tools edited
    Task: T324935

--- src/views/LeaderboardView.vue
+++ src/views/LeaderboardView.vue
@@ -1,3 +1,38 @@
+<script setup>
+import {ref} from 'vue';
+const userNameHeader = ref("User's Name")
+const toolsEditedHeader = ref("Number of Tools Edited")
+const profileBaseUrl = ref("https://meta.wikimedia.org/wiki/User:")
+
+const users = ref([
+            { userName: 'Frank',  toolsEdited: 12},
+            { userName: 'John doe',  toolsEdited: 12},
+            { userName: 'Jane',  toolsEdited: 12},
+            { userName: 'Wandahuhu', toolsEdited: 12},
+        ]);
+</script>
+
 <template>
-    <h1>The leaderboard goes here.</h1>
+    <div class="container-fluid lead-container">
+        <div class="header">
+          <h2>Leaderboard</h2>
+        </div>
+        <table class="leaderboard">
+          <tbody><tr>
+            <th>{{ userNameHeader }}</th>
+            <th></th>
+            <th>{{ toolsEditedHeader }}</th>
+          </tr>
+          <tr v-for="(user, id) in users" :key="id">
+            <td >
+                <a :href="`${profileBaseUrl}${user.userName.split(' ').join('_')}`">{{user.userName}}</a>
+            </td>
+            <td></td>
+            <td class="tool-edits">{{user.toolsEdited}}</td>
+          </tr>
+
+          </tbody>
+        </table>
+      </div>
 </template>
+
