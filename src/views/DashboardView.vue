59f6a09944e82306398dd307c2d3d3323b330712
nbarnabee <nicole.barnabee@gmail.com>
2023-01-05 14:12:16 +0100

    Add styles to dashboard header
    Once again Vuetify and I have not gotten along.  I'll have another look
    at this at a later date and see if I can convert it from vanilla CSS.

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,3 +1,51 @@
 <template>
-  <h1>The dashboard goes here.</h1>
+    <section class="dashboard-header">
+          <img src="src/assets/logo-main.svg" height="100" width="100" alt=""/>
+          <div>
+            <h1>Welcome back, {{ username }}!</h1>
+            <p>
+              Here on the Dashboard, you can view your latest contributions and check out global contributions and statistics about Toolhub and the Toolhunt project.
+            </p>
+          </div>
+    </section>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        username: "NicoleLBee"
+      }
+    }
+  }
+</script>
+
+<style>
+  .dashboard-header {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+    max-width: 800px;
+    margin-inline: auto;
+  }
+
+  .dashboard-header p {
+    display: none;
+  }
+
+  .dashboard-header img {
+    height: 50px;
+    width: 50px;
+  }
+
+  @media (min-width: 600px) {
+    .dashboard-header p {
+      display: block;
+    }
+
+    .dashboard-header img {
+      height: 100px;
+      width: 100px;
+    }
+  }
+</style>
