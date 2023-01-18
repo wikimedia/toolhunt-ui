5cb5d51df47641129004cc51f1353c00770a1ec1
Hannah Waruguru <hannah.waruguru8@gmail.com>
2023-01-18 12:59:54 +0300

    HomeView: Add home page (#16)
    * HomeView: Add home page
    
    Add the table with the toolnames and missing fields
    add the home page contents
    
    Task: T326870
    
    * HomeView: Add the display
    
    Add displays for different screen
    sizes to make the page more responsive.
    
    Task: T326870
    
    * HomeView: Add Vuetify themes
    
    Add vuetify theme colors
    Import the vuetify instance to main.js
    
    Task: T326870
    
    * HomeView: Minor changes
    
    Remobve the search bar
    
    Task: T326870

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,13 +1,48 @@
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
-import { aliases, mdi } from "vuetify/iconsets/midi";
+import '@mdi/font/css/materialdesignicons.css';
+
+const customLightTheme = {
+  dark: false,
+  colors: {
+  	/* Vuetify theme settings */
+    primary: '#36c', /* WMF: accent50 */
+    secondary: '#202122', /* WMF: base10 */
+    accent: '#f8f9fa', /* WMF: base90 */
+    error: '#d33', /* WMF: red50 */
+    info: '#ac6600', /* WMF: yellow30 */
+    success: '#00af89', /* WMF: green50 */
+    warning: '#fc3', /* WMF: yellow50 */
+
+    /* Additional Wikimedia style guide colors by name */
+    base10: '#202122',
+    base20: '#54595d',
+    base30: '#72777d',
+    base80: '#e0e0e0',
+    base90: '#f8f9fa',
+    base100: '#fff',
+    accent90: '#eaf3ff',
+    accent50: '#36c',
+    red50: '#d33',
+    green50: '#00af89',
+    yellow30: '#ac6600',
+    yellow50: '#fc3',
+    yellow90: '#fef6e7',
+
+    /* Colors from Toolhub logo */
+    logoblue: '#069',
+    logogreen: '#396',
+    logored: '#900'
+  },
+};
 
 export default createVuetify({
-  icons: {
-    defaultSet: "mdi",
-    aliases,
-    sets: {
-      mdi,
-    },
-  },
+  theme: {
+    defaultTheme: 'customLightTheme',
+    themes: {
+      customLightTheme
+    }
+  }
 });
+
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,125 @@
 <script setup>
+import { ref } from "vue";
+
+const tasks = ref({
+  toolName: "Pywikibot",
+  toolDescription:
+    "Python library and collection of scripts that automate work on MediaWiki sites",
+  url: "https://www.mediawiki.org/wiki/Special:MyLanguage/Manual:Pywikibot",
+  missingField: "wikidata_qid",
+});
 </script>
 
 <template>
-    <h1>Toolhunt home page</h1>
+  <v-container>
+    <!-- display for tablet and above -->
+    <v-row class="hidden-xs">
+      <v-col cols="2" class="d-flex justify-start align-center">
+        <v-img
+          src="./src/assets/logo-main.svg"
+          height="100"
+          width="100"
+          class="overflow-visible"
+        ></v-img>
+      </v-col>
+      <v-col
+        cols="10"
+        class="d-flex justify-center align-start flex-column pl-1"
+      >
+        <div class="text-h4">Toolhunt</div>
+        <div class="text-subtitle-2">
+          Currently, many tools in Toolhub have missing or incomplete
+          information.
+        </div>
+        <div class="text-subtitle-2">
+          Please help us add the missing fields and incomplete information
+        </div>
+      </v-col>
+    </v-row>
+    <!-- display for mobile only -->
+    <v-row class="hidden-sm-and-up">
+      <v-col cols="2" class="d-flex justify-start align-center pl-1">
+        <v-img
+          src="./src/assets/logo-main.svg"
+          class="overflow-visible"
+        ></v-img>
+      </v-col>
+      <v-col
+        cols="6"
+        class="d-flex justify-center align-start flex-column pl-1"
+      >
+        <div class="text-h4">Toolhunt</div>
+      </v-col>
+    </v-row>
+    <v-row class="hidden-sm-and-up">
+      <v-col col="12">
+        <div class="text-subtitle-2">
+          Currently, many tools in Toolhub have missing or incomplete
+          information.
+        </div>
+        <div class="text-subtitle-2">
+          Please help us add the missing fields and incomplete information
+        </div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-card-item>
+            <v-card-title>Help us add missing fields</v-card-title>
+          </v-card-item>
+          <v-card-text>
+            <v-row>
+              <v-col cols="12">
+                <p>
+                  Please click on the
+                  <strong
+                    ><span class="bg primary theme--light"
+                      >Yes Please</span
+                    ></strong
+                  >
+                  button to add the missing fields and incomplete information.
+                </p>
+                <p>
+                  You can also click the
+                  <strong><span class="bg primary">Skip to Next</span></strong>
+                  to see if you would prefer to add the next missing field
+                </p>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <v-table class="table elevation-2">
+                  <tbody>
+                    <tr>
+                      <td>Tool Name</td>
+                      <td>{{ tasks.toolName }}</td>
+                    </tr>
+                    <tr>
+                      <td>Tool Description</td>
+                      <td>{{ tasks.toolDescription }}</td>
+                    </tr>
+                    <tr>
+                      <td>Url</td>
+                      <td>{{ tasks.url }}</td>
+                    </tr>
+                    <tr>
+                      <td>Missing Field Name</td>
+                      <td>{{ tasks.missingField }}</td>
+                    </tr>
+                  </tbody>
+                </v-table>
+              </v-col>
+            </v-row>
+          </v-card-text>
+          <v-card-action
+            class="d-flex justify-center justify-space-around flex-row"
+          >
+            <v-btn class="my-2" color="primary base100--text theme--light">Yes Please</v-btn>
+            <v-btn class="my-2" >Skip to Next</v-btn>
+          </v-card-action>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
