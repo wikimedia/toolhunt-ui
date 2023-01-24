21f690e56df29e2e58fcf89b38df7004d9c711f1
Hannah_Waruguru <hannah.waruguru8@gmail.com>
2023-01-24 23:18:36 +0300

    Leaderboard.spec: Minor Changes
    Task: T327236

--- src/components/__tests__/LeaderboardTable.spec.js
+++ src/components/__tests__/LeaderboardTable.spec.js
@@ -25,20 +25,19 @@ describe("LeaderboardTable", () => {
     });
     const tbody = wrapper.find("tbody");
     const rows = tbody.findAll("tr");
-    // Assert that the table rows is equal to the number of items in the contributions array
+
     expect(wrapper.text()).toContain("All Time Best");
     const firstRowCols = rows[0].findAll("td");
-    // Assert that the anchor text containing the username is present as a child of the first cell in the row
-    // and it renders the correct text.
+   
     const firstCol = firstRowCols[0].find("a");
     expect(firstCol.text()).toBe("Hannah Waruguru");
     expect(firstCol.attributes().href).toBe(
       "https://meta.wikimedia.org/wiki/User:Hannah_Waruguru"
     );
-    // Assert that the text containing the contribution number for the user is in the second cell in the row
+
     const contributionCol = firstRowCols[1];
     expect(contributionCol.text()).toBe("12");
-    // test that it's rendering one row
+   
     expect(rows.length).toBe(LeaderboardTableProps.contributions.length);
   });
 });
