4b2aced191b02bcb674b904b9833a001ca94a1c4
Hannah_Waruguru <hannah.waruguru8@gmail.com>
2023-03-03 12:46:03 +0300

    .env: Add the BASE_URL variable
    Task: T331101

--- src/stores/api.js
+++ src/stores/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:8082'
+const BASE_URL = import.meta.env.VITE_BASE_URL
 
 export async function getTasks() {
     const res = await axios.get(BASE_URL + '/api/tasks', {raw:true})
