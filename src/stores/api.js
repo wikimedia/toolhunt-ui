8e3a875494c5d91d83fb2a0646e2ac3f47b5f80e
Hannah_Waruguru <hannah.waruguru8@gmail.com>
2023-03-03 14:56:47 +0300

    .env: Add a variable to .env
    Task: T331101

--- src/stores/api.js
+++ src/stores/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_BASE_URL
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 export async function getTasks() {
     const res = await axios.get(BASE_URL + '/api/tasks', {raw:true})
