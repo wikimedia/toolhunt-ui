c9bc89064c2b388cea88521a37d9b791f6be106d
Hannah Waruguru <hannah.waruguru8@gmail.com>
2023-03-06 00:26:04 +0300

    Add .env (#38)
    * .env: Add the BASE_URL variable
    
    Task: T331101
    
    * .env: Add a variable to .env
    
    Task: T331101

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,48 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vuetify({ autoImport: true })],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  server: {
-    watch: {
-      usePolling: true, // needed for hot reloads
-    },
-    host: true, // needed for the Docker Container port mapping to work
-    hmr: {
-      clientPort: 8082,
-    },
-    strictPort: true,
-    port: 8082, // needs to be specified
-    proxy: {
-      // string shorthand: http://localhost:8082/api -> http://localhost:5000/api
-      '/api': 'http://flask-web:5000',
-      // string shorthand: http://localhost:8082/openapi.json -> http://localhost:5000/openapi.json
-      '/openapi.json': {
-        target: 'http://flask-web:5000',
-        changeOrigin: true,
+
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return defineConfig({
+    plugins: [vue(), vuetify({ autoImport: true })],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
-    }
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    deps: {
-      inline: ["vuetify"],
     },
-  },
-});
+    server: {
+      watch: {
+        usePolling: true, // needed for hot reloads
+      },
+      host: true, // needed for the Docker Container port mapping to work
+      hmr: {
+        clientPort: 8082,
+      },
+      strictPort: true,
+      port: 8082, // needs to be specified
+      proxy: {
+        // string shorthand: http://localhost:8082/api -> http://localhost:5000/api
+        '/api': process.env.VITE_FLASK_BACKEND_URL,
+        // string shorthand: http://localhost:8082/openapi.json -> http://localhost:5000/openapi.json
+        '/openapi.json': {
+          target: process.env.VITE_FLASK_BACKEND_URL,
+          changeOrigin: true,
+        },
+      }
+    },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      deps: {
+        inline: ["vuetify"],
+      },
+    },
+  });
+  
+}
+  
