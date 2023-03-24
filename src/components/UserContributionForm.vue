c18a86e3dfd38f38b4642b65910eb38801654447
nbarnabee <nicole.barnabee@gmail.com>
2023-03-24 17:06:41 +0100

    Narrow the scope of console.log messages
    Console logging the entire error was spamming the console with
    massive amounts of useless information.
    
    My linter also inserted some semi-colons, thereby fixing a
    formatting inconsistency.

--- src/components/UserContributionForm.vue
+++ src/components/UserContributionForm.vue
@@ -66,7 +66,7 @@ const submit = async () => {
           );
         });
     } catch (error) {
-      console.log(error);
+      console.log(error.message);
     } finally {
       missingFieldValue.value = null;
     }
