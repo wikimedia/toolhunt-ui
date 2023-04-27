11e683de9c3c5150da3421f577c09535b8a6b913
Nicole Barnabee <95652414+nbarnabee@users.noreply.github.com>
2023-04-27 16:35:20 +0200

    Add "remove on submit" functionality (#41)
    I've merged several commits into this one:
    1. Refactor Home.vue and UserContributionForm.vue to be sibling
    components
    2. Move functionality from Home.vue to HomeView.vue (this allows the
    parent component, HomeView, to communicate directly with
    UserContributionForm)
    3. Rewrite the test for the Home.vue component.

--- src/components/__tests__/Home.spec.js
+++ src/components/__tests__/Home.spec.js
@@ -8,24 +8,10 @@ describe("Home", () => {
   const profileBaseUrl = "https://meta.wikimedia.org/wiki/User:";
   it("renders properly", () => {
     const HomeProps = {
-      tasks: [
-        {
-          field: {
-            description: "a thing for doing the thing",
-            input_options: null,
-            name: "wikidata_qid",
-          },
-          id: 1,
-          timestamp: null,
-          tool: {
-            description: "Shows you little things you can do on Wikidata.",
-            name: "mm_wikidata_todo",
-            title: "Wikidata Todo",
-            url: "http://tools.wmflabs.org/wikidata-todo",
-          },
-          user: null,
-        },
-      ],
+      toolName: "mm_wikidata_todo",
+      toolDescription: "Shows you little things you can do on Wikidata.",
+      missingField: "wikidata_qid",
+      toolURL: "http://tools.wmflabs.org/wikidata-todo",
     };
     const wrapper = mount(Home, {
       props: HomeProps,
@@ -34,7 +20,7 @@ describe("Home", () => {
       },
     });
     const rows = wrapper.find("tbody").findAll("tr");
-    expect(rows[0].findAll("td")[1].text()).toBe("Wikidata Todo");
+    expect(rows[0].findAll("td")[1].text()).toBe("mm_wikidata_todo");
     expect(rows[1].findAll("td")[1].text()).toBe(
       "Shows you little things you can do on Wikidata."
     );
@@ -46,42 +32,12 @@ describe("Home", () => {
 
   it("shows next task when skip next is clicked", async () => {
     const HomeProps = {
-      tasks: [
-        {
-          field: {
-            description: "a thing for doing the thing",
-            input_options: null,
-            name: "wikidata_qid",
-          },
-          id: 1,
-          timestamp: null,
-          tool: {
-            description:
-              "Python library and collection of scripts that automate work on MediaWiki sites",
-            name: "Pywikibot",
-            title: "Pywikibot",
-            url: "https://www.mediawiki.org/wiki/Special:MyLanguage/Manual:Pywikibot",
-          },
-          user: null,
-        },
-        {
-          field: {
-            description: "a thing for doing the thing",
-            input_options: null,
-            name: "openhub_id",
-          },
-          id: 1,
-          timestamp: null,
-          tool: {
-            description:
-              "This project is aimed at getting very basic author statistics for a specified page.",
-            name: "toolforge-authors",
-            title: "Authors of a page",
-            url: "https://toolsadmin.wikimedia.org/tools/id/authors",
-          },
-          user: null,
-        },
-      ],
+      toolName: "Pywikibot",
+      toolDescription:
+        "Python library and collection of scripts that automate work on MediaWiki sites",
+      missingField: "wikidata_qid",
+      toolURL:
+        "https://www.mediawiki.org/wiki/Special:MyLanguage/Manual:Pywikibot",
     };
     const wrapper = mount(Home, {
       props: HomeProps,
@@ -98,19 +54,5 @@ describe("Home", () => {
       "https://www.mediawiki.org/wiki/Special:MyLanguage/Manual:Pywikibot"
     );
     expect(rows[3].findAll("td")[1].text()).toBe("wikidata_qid");
-
-    // click on Skip to Next button
-    await wrapper.findAll("button")[1].trigger("click");
-
-    // Expect the table to be updated with the next task details
-    const updatedRows = wrapper.find("tbody").findAll("tr");
-    expect(rows[0].findAll("td")[1].text()).toBe("Authors of a page");
-    expect(rows[1].findAll("td")[1].text()).toBe(
-      "This project is aimed at getting very basic author statistics for a specified page."
-    );
-    expect(rows[2].findAll("td")[1].text()).toBe(
-      "https://toolsadmin.wikimedia.org/tools/id/authors"
-    );
-    expect(rows[3].findAll("td")[1].text()).toBe("openhub_id");
   });
 });
