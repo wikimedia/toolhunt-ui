b85160c0a6a4233ce660e5be6a638527e4dcab02
nbarnabee <nicole.barnabee@gmail.com>
2023-05-19 17:28:39 +0300

    Clean extraneous tags/information and rename
    "Home.vue" did not accurately describe the component's usage
    Removed more static text
    Removed unnecessary HTML nesting

---
+++ src/components/ToolData.vue
@@ -0,0 +1,46 @@
+<script setup>
+const props = defineProps({
+  toolName: String,
+  toolDescription: String,
+  missingField: String,
+  toolURL: String,
+  isError: Boolean,
+});
+</script>
+<template>
+  <v-container>
+    <v-table class="table elevation-2">
+      <tr>
+        <th colspan="2" v-if="isError">
+          <v-alert
+            density="compact"
+            type="error"
+            text="We are having trouble fetching this data for you right now, please refresh the page"
+          ></v-alert>
+        </th>
+      </tr>
+      <tbody>
+        <tr>
+          <td>Tool Name</td>
+          <td>{{ props.toolName }}</td>
+        </tr>
+        <tr>
+          <td>Tool Description</td>
+          <td>{{ props.toolDescription }}</td>
+        </tr>
+        <tr>
+          <td>Url</td>
+          <td>
+            <a :href="props.toolURL" target="_blank">
+              {{ props.toolURL }}
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td>Missing Field Name</td>
+          <td>{{ props.missingField }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+  </v-container>
+</template>
