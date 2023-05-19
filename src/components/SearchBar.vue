9c89f6f30ebb7043b50d70d8fd778f4f2782cafd
nbarnabee <nicole.barnabee@gmail.com>
2023-05-19 17:04:31 +0300

    Improve search bar appearance

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,5 +1,16 @@
-<script></script>
+<script setup>
+function clickTest() {
+  console.log("Clicked");
+}
+</script>
 <template>
-  <v-text-field label="Search for a tool" clearable> </v-text-field>
+  <v-text-field
+    label="Search for a tool"
+    variant="outlined"
+    append-inner-icon="mdi-magnify"
+    clearable
+    @click:append-inner="clickTest"
+  >
+  </v-text-field>
 </template>
 <style scoped></style>
